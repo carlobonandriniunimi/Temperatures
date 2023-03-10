d9936e9578cf655cc5c24bf977dbedc2e148c8e2
carlobonandriniunimi <carlo.bonandrini@studenti.unimi.it>
2023-03-10 16:42:49 +0100

    Added dataset and fixed missing values

--- src/sitka_highs_lows.py
+++ src/sitka_highs_lows.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 # * Other way of opening files
-path = Path('weather_data/sitka_weather_2021_simple.csv')
+path = Path('weather_data/death_valley_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
@@ -14,13 +14,20 @@ header_row = next(reader)
 highs = []
 lows = []
 dates = []
+location = ""
 for row in reader:
+    if location == "":
+        location = row[1].title()
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = int(row[4])
-    low = int(row[5])
-    highs.append(high)
-    lows.append(low)
-    dates.append(current_date)
+    try:  # In case of missing values
+        high = int(row[header_row.index('TMAX')])
+        low = int(row[header_row.index('TMIN')])
+    except ValueError:
+        print(f"Missing data from {current_date}")
+    else:
+        highs.append(high)
+        lows.append(low)
+        dates.append(current_date)
 
 # Plot the high and low temperatures
 plt.style.use('seaborn-v0_8')
@@ -31,8 +38,8 @@ ax.plot(dates, lows, color='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot
-ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
-ax.set_xlabel('', fontsize=16)
+title = f"Daily High and Low Temperatures, 2021\n{location}"
+ax.set_title(title, fontsize=24)
 # Puts them diagonally
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
