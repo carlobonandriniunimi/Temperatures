f26d1656be738aa99384b3b1b7d3323bfd16a34c
carlobonandriniunimi <carlo.bonandrini@studenti.unimi.it>
2023-03-10 16:22:40 +0100

    Modified dataset with all 2021 temps

--- src/sitka_highs.py
+++ src/sitka_highs.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-path = Path('weather_data/sitka_weather_07-2021_simple.csv')
+# * Other way of opening files
+path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
@@ -24,7 +25,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
 
 # Format plot
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 # Puts them diagonally
 fig.autofmt_xdate()
